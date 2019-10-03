564dee3ce9a42ef6b75b2d3021a971c28f6dedc8
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-10-03 14:41:52 -0400

    Remove all old UCDM code

--- README.md
+++ README.md
@@ -1,31 +1,21 @@
-# unizin-sis-validation
-Unizin SIS Validation Scripts
+# unizin-validation
+Unizin UDW Validation Scripts
 
-These scripts are used for validating that the SIS data loaded into Unizin is correct.
+These scripts are used for validating that the UDW data loaded into Unizin is correct.
 
 To connect to this database you'll likely need a VPN or Ethernet that has been granted permission.
 
 To run this
-1. Copy the .env.sample file to .env
-2. Edit the values in this file to add your Google Cloud Database values
-3. Manually Download the CSV files that are loaded from the S3 bucket (This may be automated in the future for automated validation)
-4. If they don't match our file names, edit dbqueries.py and edit the sis_files values to match up with your csv file names
+1. Copy the .env.sample file to .env this has one entry ENV_FILE
+2. Copy the env_sample.json to some local directory or somewhere on the file system
+3. Edit the values in this file to add your Google Cloud Database values
 
 If SMTP is needed to test you can run a local SMTP server as described in sample env.
 
 `python -m smtpd -n -c DebuggingServer localhost:1025`
 
-These scripts have currently only been tested for Michigan. They also do not 100% validate yet and some of the empty columns and are a work in progress as of 2018-09-10. 
-
-Run the python script validate.py selecting first option 1 to download the data from the Postgres in Google.
-
-The run Option 2 to validate all tables. 
-
-Option 3 will run all the tables except course_section_enrollment which currently has a lot of columns that don't match up.
-
-To build this on docker you can do
-
 docker build -t unizin-sis-validate . && docker run -d --env-file .env unizin-sis-validate
 
-TODO: 
-Add automatic download from S3 bucket for SIS CSV files using boto3
+You can also run this on localhost, just pass the location of your environment file. Make sure all requirements are installed or you're in a virtual environment.
+
+`ENV_FILE=/Users/jonespm/mylasecrets/env-unizin.json python validate.py
--- validate.py
+++ validate.py
@@ -1,6 +1,6 @@
-# validate.py - Unizin UCDM and UCW validator
+# validate.py - Unizin UDW validator
 #
-# Copyright (C) 2018 University of Michigan Teaching and Learning
+# Copyright (C) 2019 University of Michigan Teaching and Learning
 
 UNIZIN_FILE_FORMAT = "unizin_{table}.csv"
 
@@ -38,95 +38,9 @@ OUT_DIR = ENV.get("TMP_DIR", "/tmp/")
 RESULTS_FILE = open(OUT_DIR + "u_results.txt", "w")
 ERRORS_FILE = open(OUT_DIR + "u_errors.txt", "w")
 
-class SimpleQuoter(object):
-    @staticmethod
-    def sqlquote(x=None):
-        return "'bar'"
-
-def lower_first(iterator):
-    return itertools.chain([next(iterator).lower()], iterator)
-
-def load_CSV_to_dict(infile, indexname):
-    df = pd.read_csv(infile, delimiter=',', dtype=str)
-    # Lower case headers
-    df.columns = map(str.lower, df.columns)
-    df[indexname] = pd.to_numeric(df[indexname], errors='coerce', downcast='integer')
-
-    df = df.fillna('NoData')
-    return df.set_index(indexname, drop=False)
-
-#def close_compare(a, b):
-#    if (isinstance(a, float) and isinstance(b,float)):
-#        return np.isclose(a,b)
-#    return a == b
-
-def close_compare(i, j):
-    try:
-        i = float(i)
-        j = float(j)
-        return np.isclose(i,j)
-    except ValueError:
-        return i==j
-
-def compare_CSV(tablename):
-    RESULTS_FILE.write(f"Comparing on {tablename}\n")
-    sis_file = dbqueries.QUERIES[tablename]['sis_file'].format(date=ENV.get("SIS_DATE"))
-    index = dbqueries.QUERIES[tablename]['index']
-    try:    
-        SIS_df = load_CSV_to_dict(sis_file.format(table=tablename), index)
-        Unizin_df = load_CSV_to_dict(OUT_DIR + UNIZIN_FILE_FORMAT.format(table=tablename), index)
-    except Exception as e:
-        print ("Exception ",e)
-        return
-
-    SIS_len = len(SIS_df)
-    Unizin_len = len(Unizin_df)
-
-#    print (SIS_df)
-#    print (Unizin_df)
-    
-    RESULTS_FILE.write ("Unizin rows: %d, SIS rows: %d\n" % (Unizin_len, SIS_len))
-
-    if len(Unizin_df) == 0 or len(SIS_df) == 0:
-        RESULTS_FILE.write(f"This table {tablename} has a empty record for at least one dataset, skipping\n")
-        return
-
-    lendiff = Unizin_len - SIS_len
-    if lendiff > 0:
-        RESULTS_FILE.write("Unizin has %d more rows than SIS for this table\n" % abs(lendiff))
-    elif lendiff < 0:
-        RESULTS_FILE.write("SIS has %d more rows than Unizin for this table\n" % abs(lendiff))
-
-    Unizin_head = list(Unizin_df)
-    print (Unizin_head)
-
-    RESULTS_FILE.flush()
-    for i, SIS_r in tqdm(SIS_df.iterrows(), total=SIS_len):
-        #Look at all the unizin headers and compare
-        indexval = SIS_r[index]
-        if not indexval:
-            continue
-        try: 
-            Unizin_r = Unizin_df.loc[indexval]
-        except:
-            #print (f"Index error on {indexval}")
-            continue
-
-#        f = np.frompyfunc(close_compare, 2, 1)
-
-        for head in Unizin_head:
-            try:
-#               f(SIS_r[head], Unizin_r[head])
-                if not close_compare(SIS_r[head], Unizin_r[head]):
-#                    RESULTS_FILE.write("type SIS %s, type Unizin %s\n" % (type(SIS_r[head]),type(Unizin_r[head])))
-                    RESULTS_FILE.write(f"{head} does not match for {indexval} SIS: {SIS_r[head]} Unizin: {Unizin_r[head]}\n")
-            except Exception as e:
-                ERRORS_FILE.write("%s\n" % str(e)) 
-                continue
-
-def load_Unizin_to_CSV(tablename):
+def load_unizin_to_csv(tablename):
     out_filename = OUT_DIR + UNIZIN_FILE_FORMAT.format(table=tablename)
-    print (f"Loading ucdm {tablename} table to {out_filename}")
+    print (f"Loading {tablename} table to {out_filename}")
     # The DSN might switch depending on the data file
     conn = psycopg2.connect(ENV.get("DSN_"+dbqueries.QUERIES[tablename]['dsn']))
     
@@ -185,46 +99,11 @@ select_tables = list(csv.reader([ENV.get("SELECT_TABLES", "academic_term")]))[0]
 
 print (select_tables)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--option", type=int, help="Run an option by number",)
-args = parser.parse_args()
+load_unizin_to_csv("number_of_courses_by_term")
+load_unizin_to_csv("unizin_metadata")
 
-option = args.option
-
-if (not option):
-    print ("""Choose an option.
-    1 = Import *All* Unizin Data from GCloud to CSV (need developer VPN or other connection setup)
-    2 = Import *select* table(s) from GCloud to CSV (need developer VPN or other connection setup)
-    3 = Load/Compare *All* CSV files
-    4 = Load/Compare *select* table(s)
-    5 = Email/Verification job data loaded into Unizin (Special Case)
-    """)
-    print ("'Select table(s)' are: ", ', '.join(select_tables))
-    option = int(input())
-
-print (f"Running option {option}")
-if option == 1:
-    for key in dbqueries.QUERIES.keys():
-        load_Unizin_to_CSV(key)
-elif option == 2:
-    for key in dbqueries.QUERIES.keys():
-        if key in select_tables:
-            load_Unizin_to_CSV(key)
-elif option == 3:
-    for key in dbqueries.QUERIES.keys():
-        compare_CSV(key)
-elif option == 4:
-    for key in dbqueries.QUERIES.keys():
-        if key in select_tables:
-            compare_CSV(key)
-elif option == 5:
-    # Only run these queries
-    load_Unizin_to_CSV("number_of_courses_by_term")
-    load_Unizin_to_CSV("unizin_metadata")
-    subject = dbqueries.QUERIES["number_of_courses_by_term"].get('query_name')
-    email_results([OUT_DIR + UNIZIN_FILE_FORMAT.format(table="unizin_metadata"),OUT_DIR + UNIZIN_FILE_FORMAT.format(table="number_of_courses_by_term")], subject=subject)
-else: 
-    print(f"{option} is not currently a valid option")
+subject = dbqueries.QUERIES["number_of_courses_by_term"].get('query_name')
+email_results([OUT_DIR + UNIZIN_FILE_FORMAT.format(table="unizin_metadata"),OUT_DIR + UNIZIN_FILE_FORMAT.format(table="number_of_courses_by_term")], subject=subject)
 
 RESULTS_FILE.close()
 ERRORS_FILE.close()
