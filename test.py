730022c593c763f122de9a0dd69c63359c88b0b8
Sam Sciolla <ssciolla@umich.edu>
2019-10-29 10:41:49 -0400

    Add test for UDP view counts query

--- test.py
+++ test.py
@@ -38,6 +38,31 @@ class TestFlagRaising(unittest.TestCase):
         result_text = validate.generate_result_text(query_dict['query_name'], checks_result.checked_output_df)
         self.assertEqual(result_text.count('<-- "not_zero" condition failed'), 2)
 
+    def test_udp_context_store_view_counts_check(self):
+        # Set up
+        udp_view_counts_df = pd.DataFrame({
+            'table_name': [
+                'entity.learner_activity',
+                'entity.course_offering',
+                'entity.course_grade',
+                'entity.academic_term',
+                'entity.annotation',
+                'entity.learner_activity_result',
+                'entity.person',
+            ],
+            'record_count': [1000, 1000, 0, 1000, 1000, 1000, 1000]
+        })
+        query_dict = QUERIES['udp_context_store_view_counts']
+        # Test
+        checks_result = validate.run_checks_on_output(query_dict['checks'], udp_view_counts_df)
+        self.assertCountEqual(
+            ['table_name', 'record_count', 'not_zero'],
+            checks_result.checked_output_df.columns.to_list()
+        )
+        self.assertTrue(checks_result.flags == ["YELLOW"])
+        result_text = validate.generate_result_text(query_dict['query_name'], checks_result.checked_output_df)
+        self.assertIn('<-- "not_zero" condition failed', result_text)
+
     def test_unizin_metadata_check(self):
         # Set up
         delta_obj = timedelta(days=-3)
